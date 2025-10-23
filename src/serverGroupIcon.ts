1fd4582b069af13d528ca82b6751431cecc1be10
Jakub Kozera <kozerakuba@gmail.com>
2025-10-23 17:47:11 +0200

    Update server group icon to outlined stroke-based 24x24 SVG for improved scaling and style

--- src/serverGroupIcon.ts
+++ src/serverGroupIcon.ts
@@ -1,10 +1,9 @@
 import * as vscode from 'vscode';
 
 export function createServerGroupIcon(color: string): vscode.Uri {
-    const svg = `<svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-        <path fill="${color}" d="M2 2v12h12V2H2zm1 1h10v10H3V3z"/>
-        <path fill="${color}" d="M4 4h8v1H4V4zm0 2h8v1H4V6zm0 2h6v1H4V8zm0 2h8v1H4v-1z"/>
-        <circle cx="12.5" cy="5.5" r="1.5" fill="${color}" opacity="0.8"/>
+
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="${color}" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
+        <path d="M5 4h4l3 3h7a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2"/>
     </svg>`;
 
     // Create data URI
